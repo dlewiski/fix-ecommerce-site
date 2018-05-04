a9e15ddd9b28e000f00dbb5fafae270aadf14747
David Lewis <davidlewisp@gmail.com>
2018-05-04 15:48:16 -0700

    work on removing item from cart after clicking delete

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -23,7 +23,11 @@ class OrderItemsController < ApplicationController
     @item = @order.order_items.find(params[:id])
     @item.destroy
     @order.save
-    redirect_to cart_path
+    session[:order_id] = @order.id
+    respond_to do |format|
+      format.html { redirect_to cart_path }
+      format.js { render :file => "carts/show.js.erb" }
+    end
   end
 
   private
