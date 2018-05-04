23ce9ef098bb65d9c4e357000d1186db685c01e1
David Lewis <davidlewisp@gmail.com>
2018-05-04 14:27:52 -0700

    updated seed file to clear the products first and then re seed the database with product images

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,15 @@
 class Seed
 
   def self.begin
+    Product.destroy_all
     seed = Seed.new
     seed.generate_products
+    p "Created #{Product.count} products"
   end
 
   def generate_products
     20.times do |i|
-      Product.create!(name: Faker::Lorem.word, price: rand(10...100), description: Faker::Lorem.sentence(5, false, 0).chop)
+      Product.create!(name: Faker::Lorem.word, price: rand(10...100), description: Faker::Lorem.sentence(5, false, 0).chop, image: Faker::Avatar.image)
     end
   end
 end
